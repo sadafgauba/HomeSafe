fba2868545c205977e0b9abfcfa331312f6a711e
luis <luis.velasquez@utec.edu.pe>
2022-10-02 19:12:23 -0500

    change door and storage

--- Door.java
+++ Door.java
@@ -1,34 +1,35 @@
 import java.util.Scanner;  // Import the Scanner class
 public class Door {
+    public boolean sensor_status = false;
     /* 
      * The DOOR object has access and control over the physical devices of the Solenoid Motor and the Sensor Door. 
      * All interaction with these devices must go through the DOOR.
      */
 
 
-    static void door_control(boolean order_to_door ,boolean door_status) { //This function opens or closes the door depending of the comparison of the local and stored passcode
+    public void door_control(boolean order_to_door) { //This function opens or closes the door depending of the comparison of the local and stored passcode
 
-        if (order_to_door && door_status){
+        if (order_to_door && sensor_status){
 
             unlock();
             openedDoor();
 
 
 
-        } else if (!order_to_door && !door_status) {
+        } else if (!order_to_door && !sensor_status) {
 
             lock();
             lockedDoor();
 
         }
-        else if (order_to_door && !door_status) {
+        else if (order_to_door && !sensor_status) {
             //lock();
             //lockedDoor();
             System.out.println("DOOR already opened");
 
         }
 
-        else if (!order_to_door && door_status) {
+        else if (!order_to_door && sensor_status) {
 
             System.out.println("DOOR already closed");
 
@@ -43,26 +44,29 @@ public class Door {
 
 
 
-    static void lock() { //This function makes the door lock up by deactivating the solenoid.
+    void lock() { //This function makes the door lock up by deactivating the solenoid.
         System.out.println("activate solenoid");
         System.out.println("CLOSING the door");
+        sensor_status = true;
     }
 
-    static void unlock() {
+    void unlock() {
 
         System.out.println("activate solenoid");
         System.out.println("OPEN the door");
+        sensor_status = false;
 
 
     }
 
-    static void openedDoor() {
+    void openedDoor() {
         System.out.println("send notification UNLOCK");
 
 
     }
 
-    static void lockedDoor() {
+    void lockedDoor() {
+
         System.out.println("send notification LOCK");
     }
 
@@ -70,21 +74,23 @@ public class Door {
 
 
     public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+       /* Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("order_to_door");
 
-        String order_to_door = myObj.nextLine();  // Read user input
+        //String order_to_door = myObj.nextLine();  // Read user input
+        boolean order_to_door = Storage.PINcheck;
+        boolean door_status = Storage.a2;
         System.out.println("order_to_door " + order_to_door);  // Output user input
 
         System.out.println("door_status");
 
-        String door_status = myObj.nextLine();  // Read user input
+        //String door_status = myObj.nextLine();  // Read user input
         System.out.println("door_status " + door_status);  // Output user input
         System.out.println(order_to_door);
         System.out.println(door_status);
-        boolean a1=Boolean.parseBoolean(order_to_door);
-        boolean a2=Boolean.parseBoolean(door_status);
-        door_control(a1,a2);
+        //boolean a1=Boolean.parseBoolean(order_to_door);
+        //boolean a2=Boolean.parseBoolean(door_status);*/
+        //door_control(order_to_door,door_status);
 
     }
 
--- Storage.java
+++ Storage.java
@@ -1,4 +1,4 @@
-public class Storage {
+
 /*
     private int[] keyStored;
 
@@ -20,4 +20,63 @@ public class Storage {
 
 
 
-}
+import java.util.*;
+    public class Storage {
+        private int[] newPasscode={2,3,4,5};
+        public int[] PIN={1,2,5,4};
+        public static boolean PINcheck;
+        public boolean resetbutton=false;
+        public int[] storedPSC={1,2,3,4};
+
+        public boolean readPasscode(int[] PIN, int[] storedPSC){
+            Door door = new Door();
+            for (int i = 0; i < 4; i++) {
+                if (storedPSC[i] == PIN[i]) {
+                    i++;
+                }
+                else {
+                    PINcheck = false;
+                    System.out.println(PINcheck);
+                    door.door_control(PINcheck);
+                    return PINcheck;
+                }
+            }
+            PINcheck = true;
+            System.out.println(PINcheck);
+            door.door_control(PINcheck);
+
+            return PINcheck;
+        }
+
+        public void resetPasscode(boolean resetbutton){
+            if (resetbutton) {
+                writePasscode(newPasscode,storedPSC);
+                System.out.println("Password changed");
+                System.out.println(Arrays.toString(storedPSC));
+            }
+            else {
+                System.out.println("reset button not pressed");
+                return;
+            }
+        }
+        public void writePasscode(int[] newPasscode,int[] storedPSC){
+            for (int i = 0; i < 4; i++) {
+                storedPSC[i] = newPasscode[i];
+            }
+            return;
+        }
+
+
+
+
+
+    public static void main(String args[]) {
+        Storage ps = new Storage();
+        ps.readPasscode(ps.PIN, ps.storedPSC);
+        ps.resetPasscode(ps.resetbutton);
+
+
+    }
+    }
+
+
